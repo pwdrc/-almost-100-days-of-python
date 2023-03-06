7807e0ac9163c41e57543e3e30c8e95a28e9bc57
pwdrc <98365041+pwdrc@users.noreply.github.com>
2023-03-06 17:03:43 +0000

    Password generator - day 5 - 100 day of python course

--- 5-PasswordGenerator/main.py
+++ 5-PasswordGenerator/main.py
@@ -1,5 +1,6 @@
 import random
 import string
+from random import sample
 
 alfabeto = list(string.ascii_letters)
 numeros = list(range(0,10))
@@ -7,24 +8,23 @@ especiais = ['!','@','#','$','%','&']
 
 numero_letras = int(input("Quantidade de letras:"))
 numero_numeros = int(input("Quantidade de números:"))
-numero_especiais = int(input("Quantidade de caracteres especiais ('!','@','#','$','%','&'):"))
+numero_especiais = int(input("Quantidade de caracteres especiais:"))
 
-total = numero_letras + numero_numeros + numero_especiais
 password = ""
-cl = cn = ce = 0
 
-for i in range(0, total + 1):
-    factor = random.randint(0,3)
-    if (factor == 0 and cl <= numero_letras):
-        password += str(alfabeto[random.randint(0, len(alfabeto))])
-        cl = cl + 1
-    elif (factor == 1 and cn <= numero_numeros):
-        password += str(numeros[random.randint(0, len(numeros))])
-        cn = cn + 1
-    elif (factor == 2 and ce <= numero_especiais):
-        password += str(especiais[random.randint(0, len(especiais))])
-        ce = ce + 1
-    else:
-        i = i - 1
+for i in range(0,numero_letras):
+    password += str(alfabeto[random.randint(0,len(alfabeto)-1)])
 
-print(password)
+for i in range(0,numero_numeros):
+    password += str(numeros[random.randint(0,len(numeros)-1)])
+
+for i in range(0,numero_especiais):
+    password += str(especiais[random.randint(0,len(especiais)-1)])
+
+password = sample(password,len(password))
+
+pwd = ''
+for i in range(len(password)):
+    pwd += str(password[i])
+
+print(pwd)

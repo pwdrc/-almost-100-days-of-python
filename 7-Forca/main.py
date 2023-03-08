f8c7f14ec16fa0f5eb8ab31b461e01c04a0a2824
pwdrc <98365041+pwdrc@users.noreply.github.com>
2023-03-08 20:05:08 +0000

    chuta letra + verificacao

--- 7-Forca/main.py
+++ 7-Forca/main.py
@@ -1,24 +1,26 @@
 lista_de_palavras = ["azul", "banana", "casamento", "distopia", "expurgo", "financiamento", "garganta", "horta", "incorporado", "jovem"]
 
 import random
-# palavra_escolhida = lista_de_palavras[random.randint(0, len(lista_de_palavras) - 1)]
-palavra_escolhida = random.choice(lista_de_palavras)
+palavra_escolhida = lista_de_palavras[random.randint(0, len(lista_de_palavras) - 1)]
+# palavra_escolhida = random.choice(lista_de_palavras)
 
-print(f'Teste - palavra escolhida: {palavra_escolhida}')
+#print(f'Teste - palavra escolhida: {palavra_escolhida}')
 
-display = ""
+display = []
 for letra in palavra_escolhida:
-    display += "_"
-print(f'A palavra tem {len(palavra_escolhida)} letras - {display}')
+    display.append('_')
+#print(f'A palavra tem {len(palavra_escolhida)} letras - {display}')
 
-chute = input("Chuta uma letra ai: ").lower()
+print (' '.join(display))
+
+chute = input("Chuta uma letra ai, meu chapa: ").lower()
 
 i = 0
 for letra in palavra_escolhida:
-    if letra == chute:
-        display[i] = str(letra)
-    #else:
-    #    print("errooooou!") 
-    i += 1
+    if chute == letra:
+        display[i] = chute
+        i += 1
+    else:
+        i += 1
 
-print(display)
+print(' '.join(display))
